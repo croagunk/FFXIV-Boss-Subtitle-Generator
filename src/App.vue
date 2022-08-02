c0362a8b97e7f0ec81fcd25c77303fb60c297582
croagunk <39361666+croagunk@users.noreply.github.com>
2022-08-03 01:52:18 +0900

    Add MainComponent

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <HeaderComponent />
+  <MainComponent />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     HeaderComponent,
+    MainComponent,
   },
 });
 </script>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <main></main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainComponent",
+  components: {},
+});
+</script>
+
+<style lang="stylus"></style>
