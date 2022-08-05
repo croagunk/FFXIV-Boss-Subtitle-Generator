5fb80532031f448c328974ca101999ef377f5a8e
croagunk <39361666+croagunk@users.noreply.github.com>
2022-08-06 01:06:09 +0900

    Add dependencies
    - UIkit

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ import App from './App.vue'
 import store from './store'
 
 createApp(App).use(store).mount('#app')
+
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
+import 'uikit/dist/css/uikit.min.css'
+UIkit.use(Icons)
